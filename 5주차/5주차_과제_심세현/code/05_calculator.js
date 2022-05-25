03c36d78d039f8474ef14606e76cfc1b1a58ad47
Nakyeon Ko <59277499+nakyeonko@users.noreply.github.com>
2022-05-25 22:21:44 +0900

    Update 05_calculator.js

--- 5주차/5주차_과제_심세현/code/05_calculator.js
+++ 5주차/5주차_과제_심세현/code/05_calculator.js
@@ -1,22 +1,21 @@
 class Calculator {
     constructor() {
-      this.operatersArray = ["+", "-"]; 
-      this.formulaArray = [
-        ["+", (a, b) => +a + +b],
-        ["-", (a, b) => a - b],
-      ]; // 연산 함수들이 formulaArray에 저장됨
+      this.operatorsArray = [
+        {id:"+", function:(a, b) => +a + +b},
+        {id:"-", function:(a, b) => a - b}
+      ]; // 연산 함수들이 operatorArray에 저장됨
     }
   
     calculate(sentence) {
       let [a, operator, b] = sentence.split(" "); // [1, +, 1]
-      let index = this.operatersArray.indexOf(operator); // 0
-      let formulaFunction = this.formulaArray[index][1]; // (a, b) => +a + +b
-      return formulaFunction(a, b);
+      let operatorFunc = this.operatorsArray.find(element => element.id === operator )// (a, b) => +a + +b
+      return operatorFunc.function(a, b);
     }
   
     addMethod(operator, sentence) {
-      this.operatersArray.push(operator);
-      this.formulaArray.push([operator, sentence]);
+      this.operatorsArray.push({
+        id:operator, function:sentence
+      });
     }//push를 통해 새로운 연산을 추가함.
   }
   
@@ -32,16 +31,24 @@ class Calculator {
   let result = powerCalc.calculate("2 ** 3");
   alert(result); // 8
   
-  // (function () {
-  //   const calcLog = [];
-  //   while (true) {
-  //     let userInput = prompt("수식을 입력하세요 0 입력시 종료");
-  //     if (userInput === "0") {
-  //       console.log(calcLog);
-  //       return alert("End");
-  //     }
-  //     let calcResult = powerCalc.calculate(userInput);
-  //     alert(calcResult);
-  //     calcLog.push([userInput, calcResult]);
-  //   }
-  // })();
+
+
+
+  (function () {
+    const calcLog = [];
+    while (true) {
+      let userInput = prompt("수식을 입력하세요 0 입력시 종료");
+      if (userInput === "0") {
+        console.log(calcLog);
+        return alert("End");
+      }
+      let calcResult = powerCalc.calculate(userInput);
+      alert(calcResult);
+      calcLog.push([userInput, calcResult]);
+    }
+  })();
+
+  // prompt 로 수식을 입력 받고 ex) 1+1
+  // 그 수식의 계산 결과 값을 alert로 출력함 ex) 2
+  // 그리고 다시 수식을 입력받고 계산을 결과 값을 출력하는 과정이 반복됨.
+  // prompt에 0 입력시 end를 alet로 출력하고 반복이 중지됨.
